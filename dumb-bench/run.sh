c2ab1e3f0d1fd54a384cf2d9e1a565326aad708b
ALex Nikonov <alex@sshaman.ru>
2018-09-20 19:49:34 +0300

    feat: extend benchmark list with cryptonight (CPU) (#13)

--- dumb-bench/run.sh
+++ dumb-bench/run.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 sleep 3
-echo '{"results":{"cpu-sysbench-single":{"result":'$RANDOM'},"cpu-sysbench-ultra":{"result":'$RANDOM'},"cpu-sysbench-multi":{"result":'$RANDOM'},"net-download":{"result":'$RANDOM'},"net-upload":{"result":'$RANDOM'},"gpu-eth-hashrate":{"result":'$RANDOM'},"gpu-cash-hashrate":{"result":'$RANDOM'},"gpu-redshift":{"result":'$RANDOM'}}}'
+echo '{"results":{"cpu-sysbench-single":{"result":'$RANDOM'},"cpu-sysbench-ultra":{"result":'$RANDOM'},"cpu-sysbench-multi":{"result":'$RANDOM'},"net-download":{"result":'$RANDOM'},"net-upload":{"result":'$RANDOM'},"gpu-eth-hashrate":{"result":'$RANDOM'},"gpu-cash-hashrate":{"result":'$RANDOM'},"gpu-redshift":{"result":'$RANDOM'},"cpu-cryptonight":{"result":'$RANDOM'}}}'
