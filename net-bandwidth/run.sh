3e725d7651f5493bab25a81159871701d57387ac
ALex Nikonov <alex@sshaman.ru>
2019-05-02 00:01:41 +0300

    chore: properly name network test

--- net-bandwidth/run.sh
+++ net-bandwidth/run.sh
@@ -1,18 +1,14 @@
 #!/bin/sh
 
-set -ae
-
-SERVER="138.68.91.1"
-TIMEOUT="15s"
 UPLOAD=0
 DOWNLOAD=0
 
-iperf -c ${SERVER} --tradeoff --format k --reportstyle C --time ${TIMEOUT} > report.txt 2>&1
-ERR=$(cat report.txt | grep "connect failed" | wc -l)
+/usr/local/bin/python /test.py --json --bytes > data.json 2>&1
+ERR=$?
 
-if [[ "$ERR" -eq 0 ]]; then
-    DOWNLOAD=$(cat report.txt | awk -F "," '{print $9}' | head -1)
-    UPLOAD=$(cat report.txt | awk -F "," '{print $9}' | tail -1)
+if [[ $ERR == 0 ]]; then
+    DOWNLOAD=$(cat /data.json | jq '.download' | cut -d '.' -f 1)
+    UPLOAD=$(cat /data.json | jq '.upload' | cut -d '.' -f 1)
 fi
 
 echo '{"results":{"net-download":{"result":'${DOWNLOAD}'},"net-upload":{"result":'${UPLOAD}'}}}'
